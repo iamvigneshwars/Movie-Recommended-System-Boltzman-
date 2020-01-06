967a6a3da4aa7e5918a496f503b4e7e01b11e778
iamvigneshwars <iamvigneshwars@gmail.com>
2020-01-06 21:04:23 +0530

    Preparing the training set and the test set

--- movie_recommendation.py
+++ movie_recommendation.py
@@ -18,3 +18,11 @@ import torch.autograd.variable
 
 # Importing the dataset
 movies = pd.read_csv("ml-1m/movies.dat", sep ='::', header = None, engine = "python", encoding = "latin-1")
+users = pd.read_csv("ml-1m/users.dat", sep ='::', header = None, engine = "python", encoding = "latin-1")
+rating = pd.read_csv("ml-1m/ratings.dat", sep ='::', header = None, engine = "python", encoding = "latin-1")
+
+# preparing the training set and the test set
+training_set = pd.read_csv('ml-100k/u1.base', delimiter = "\t")
+training_set = np.array(training_set, dtype = 'int')
+test_set = pd.read_csv('ml-100k/u1.test', delimiter = "\t")
+test_set = np.array(test_set, dtype = 'int')
