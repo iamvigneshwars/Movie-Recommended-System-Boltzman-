826a272d6646e6430bc1ffdb7e25c7e6abd7d677
iamvigneshwars <iamvigneshwars@gmail.com>
2020-01-15 20:40:54 +0530

    Tweak

--- movie_recommendation.py
+++ movie_recommendation.py
@@ -114,7 +114,10 @@ for id_user in range(nb_users):
         _,v = rbm.sample_v(h)
         test_loss += torch.mean(torch.abs(vt[vt>=0] - v[vt>=0]))
         s += 1.
+
 print('test loss: '+str(test_loss/s))
+print('test loss: '+str(test_loss/s))
+
 
 
 
